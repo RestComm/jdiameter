c4ea3ee7288c66a99f4c45f9a89cca8aa63c1685
Alexandre Mendonça <ammendonca>
2008-12-08 12:08:13 +0000

    Improvements to AVP Representation and Dictionary.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@3983 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/mux/jar/src/main/java/org/mobicents/diameter/dictionary/AvpDictionary.java
+++ core/mux/jar/src/main/java/org/mobicents/diameter/dictionary/AvpDictionary.java
@@ -1,3 +1,26 @@
+/*
+ * Mobicents, Communications Middleware
+ * 
+ * Copyright (c) 2008, Red Hat Middleware LLC or third-party contributors as
+ * indicated by the @author tags or express copyright attribution statements
+ * applied by the authors. All third-party contributions are distributed under
+ * license by Red Hat Middleware LLC.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.mobicents.diameter.dictionary;
 
 import java.io.FileInputStream;
@@ -14,42 +37,50 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+/**
+ * 
+ * AvpDictionary.java
+ *
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a> 
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
+ * @version 1.0 
+ */
 public class AvpDictionary
 {
   private static Logger logger = Logger.getLogger( AvpDictionary.class );
-  
+
   public final static AvpDictionary INSTANCE = new AvpDictionary();
-  
+
   private static HashMap<String, AvpRepresentation> avpMap = new HashMap<String, AvpRepresentation>();
-  
+
   private static HashMap<String, String> vendorMap = new HashMap<String, String>();
 
   private static HashMap<String, String> commandMap = new HashMap<String, String>();
 
   private static HashMap<String, String> typedefMap = new HashMap<String, String>();
-  
+
   private static HashMap<String, String> nameToCodeMap = new HashMap<String, String>();
 
   private AvpDictionary() {
     // Exists only to defeat instantiation.
   }
-  
+
   public void parseDictionart(String filename) throws Exception
   {
     parseDictionary( new FileInputStream( filename ) );  
   }
-  
+
   public void parseDictionary(InputStream is) throws Exception
   {
     long startTime = System.currentTimeMillis();
-    
+
     DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
     dbf.setValidating( false );
     DocumentBuilder db = dbf.newDocumentBuilder();
     Document doc = db.parse( is );
-    
+
     doc.getDocumentElement().normalize();
-    
+
     /**************************************************************************
      *  VENDORS 
      */
@@ -71,18 +102,18 @@ public class AvpDictionary
      */
 
     NodeList vendorNodes = doc.getElementsByTagName("vendor");
-    
+
     for (int v = 0; v < vendorNodes.getLength(); v++) {
-      
+
       Node vendorNode = vendorNodes.item(v);
-      
+
       if (vendorNode.getNodeType() == Node.ELEMENT_NODE)
       {
         Element vendorElement = (Element) vendorNode;
-        
+
         String vendorCode = vendorElement.getAttribute( "code" );
         String vendorId = vendorElement.getAttribute( "vendor-id" );
-        
+
         vendorMap.put( vendorId, vendorCode );
       }
     }
@@ -90,7 +121,7 @@ public class AvpDictionary
     /**************************************************************************
      * COMMAND-CODES
      */
-    
+
     /*
      * <!-- *********************** Commands ***************************** -->
      * <!-- Diameter Base Protocol Command Codes -->
@@ -104,68 +135,68 @@ public class AvpDictionary
      * <command name="Disconnect-Peer" code="282" vendor-id="None"/>
      * <!-- ********************** End Commands ************************** -->
      */
-    
+
     NodeList commandNodes = doc.getElementsByTagName("command");
-    
+
     for (int c = 0; c < commandNodes.getLength(); c++)
     {
       Node vendorNode = commandNodes.item(c);
-      
+
       if (vendorNode.getNodeType() == Node.ELEMENT_NODE)
       {
         Element vendorElement = (Element) vendorNode;
-        
+
         String commandName = vendorElement.getAttribute( "name" );
         String commandCode = vendorElement.getAttribute( "code" );
         String commandVendorId = vendorElement.getAttribute( "vendor-id" );
-        
+
         String commandVendorCode = vendorMap.get( commandVendorId );
-        
+
         commandMap.put( commandVendorCode + "-" + commandCode, commandName );
       }
     }
-    
+
 
     /**************************************************************************
      * TYPE DEFINITIONS
      */
-    
+
     /*
      * <typedefn type-name="OctetString"/>
      * <typedefn type-name="UTF8String" type-parent="OctetString"/>
      * <typedefn type-name="VendorId" type-parent="Unsigned32"/>
      */
-    
+
     NodeList typedefNodes = doc.getElementsByTagName("typedefn");
-    
+
     for (int td = 0; td < typedefNodes.getLength(); td++)
     {
       Node typedefNode = typedefNodes.item(td);
-      
+
       if (typedefNode.getNodeType() == Node.ELEMENT_NODE)
       {
         Element typedefElement = (Element) typedefNode;
-        
+
         String typeName = typedefElement.getAttribute( "type-name" );
         String typeParent = typedefElement.getAttribute( "type-parent" );
-        
+
         if(typeParent.equals(""))
           typeParent = typeName;
-        
+
         typedefMap.put(typeName, typeParent);
       }
     }
-    
+
     /**************************************************************************
      *  AVPs 
      */
-    
+
     NodeList avpNodes = doc.getElementsByTagName("avp");
-    
+
     for (int s = 0; s < avpNodes.getLength(); s++) {
 
       Node avpNode = avpNodes.item(s);
-      
+
       /*
        * <!ELEMENT avp ((type | grouped), (enum*))>
        * <!ATTLIST avp
@@ -183,38 +214,38 @@ public class AvpDictionary
       if (avpNode.getNodeType() == Node.ELEMENT_NODE)
       {
         Element avpElement = (Element) avpNode;
-        
+
         String avpName = avpElement.getAttribute("name");
-        
+
         String avpDescription = avpElement.getAttribute("description");
-        
+
         String avpCode = avpElement.getAttribute("code");
-        
+
         String avpMayEncrypt = avpElement.getAttribute("may-encrypt");
-        
+
         String avpMandatory = avpElement.getAttribute("mandatory");
-        
+
         String avpProtected = avpElement.getAttribute("protected").equals("") ? "may" : avpElement.getAttribute("protected");
-        
+
         String avpVendorBit = avpElement.getAttribute("vendor-bit");
-        
+
         String avpVendorId = avpElement.getAttribute("vendor-id");
-        
+
         String avpConstrained = avpElement.getAttribute("constrained");
-        
+
         String avpType = "";
-        
+
         // Now either we have type or grouped
         NodeList avpChildNodes = avpNode.getChildNodes();
-        
+
         for (int j = 0; j < avpChildNodes.getLength(); j++)
         {
           Node avpChildNode = avpChildNodes.item(j);
-          
+
           if (avpChildNode.getNodeType() == Node.ELEMENT_NODE)
           {
             Element avpChildElement = (Element) avpChildNode;
-           
+
             if(avpChildElement.getNodeName().equals("grouped"))
             {
               // All we need to know is that's a grouped AVP.
@@ -227,34 +258,42 @@ public class AvpDictionary
             }
           }
         }
-        
+
         if(logger.isDebugEnabled())
         {
           logger.debug("Parsed AVP: Name[" + avpName + "] Description[" + avpDescription + "] Code[" + avpCode + "] May-Encrypt[" + avpMayEncrypt + "] Mandatory[" + avpMandatory +
               "] Protected [" + avpProtected + "] Vendor-Bit [" + avpVendorBit + "] Vendor-Id [" + avpVendorId + "] Constrained[" + avpConstrained + "] Type [" + avpType + "]");
         }
-        
-        AvpRepresentation avp = new AvpRepresentation(avpName, avpDescription, Integer.valueOf(avpCode), 
-            avpMayEncrypt.equals("yes"), avpMandatory, avpProtected, avpVendorBit, getVendorCode(avpVendorId), 
-            avpConstrained.equals("true"), avpType);
-        
-        avpMap.put( avp.getVendorId() + "-" + avp.getCode(), avp );
-        
-        nameToCodeMap.put( avp.getName(), avp.getVendorId() + "-" + avp.getCode() );
+        try{
+          AvpRepresentation avp=null;
+
+          avp = new AvpRepresentation(avpName, avpDescription, Integer.valueOf(avpCode), 
+              avpMayEncrypt.equals("yes"), avpMandatory, avpProtected, avpVendorBit, getVendorCode(avpVendorId), 
+              avpConstrained.equals("true"), avpType);
+
+          avpMap.put( avp.getVendorId() + "-" + avp.getCode(), avp );
+
+          nameToCodeMap.put( avp.getName(), avp.getVendorId() + "-" + avp.getCode() );
+        }catch(Exception e)
+        {
+          logger.error("Parsed AVP: Name[" + avpName + "] Description[" + avpDescription + "] Code[" + avpCode + "] May-Encrypt[" + avpMayEncrypt + "] Mandatory[" + avpMandatory +
+              "] Protected [" + avpProtected + "] Vendor-Bit [" + avpVendorBit + "] Vendor-Id [" + avpVendorId + "] Constrained[" + avpConstrained + "] Type [" + avpType + "]");
+          e.printStackTrace();
+        }
       }
     }
-    
+
     long endTime = System.currentTimeMillis();
-    
+
     logger.info( "AVP Dictionary :: Loaded in " + (endTime-startTime) + "ms == Vendors[" + vendorMap.size() + "] Commands[" + commandMap.size() + "] Types[" + typedefMap.size() + 
         "] AVPs[" + avpMap.size() + "]" );
   }
-  
+
   public AvpRepresentation getAvp(int code)
   {
     return getAvp( code, 0 );
   }
-  
+
   public AvpRepresentation getAvp(int code, long vendorId)
   {
     AvpRepresentation avp = avpMap.get(vendorId + "-" + code);
@@ -264,25 +303,37 @@ public class AvpDictionary
   public AvpRepresentation getAvp(String avpName)
   {
     String codeAndVendor = nameToCodeMap.get(avpName);
-    
+
     if(codeAndVendor != null)
     {
       String[] splitCodeAndVendor = codeAndVendor.split("-");
-      
+
       int vendor = Integer.valueOf(splitCodeAndVendor[0]);
       int code = Integer.valueOf(splitCodeAndVendor[1]);
-      
+
       return getAvp( code, vendor );
     }
-    
+
     return null;
   }
-  
-  private String getVendorCode(String vendorId)
+
+  private long getVendorCode(String vendorId)
   {
-    String vendorCode = vendorMap.get(vendorId);
+    long value = -1;
     
-    return vendorCode != null ? vendorCode : "0";
+    if(vendorId == null)
+    {
+      value = 0;
+    }
+    else
+    {
+      String vendorCode = vendorMap.get(vendorId);
+      
+      value = vendorCode == null ? 0 : Long.parseLong(vendorCode); 
+    }
+
+
+    return value;
   }
 
 }
--- core/mux/jar/src/main/java/org/mobicents/diameter/dictionary/AvpRepresentation.java
+++ core/mux/jar/src/main/java/org/mobicents/diameter/dictionary/AvpRepresentation.java
@@ -1,21 +1,58 @@
+/*
+ * Mobicents, Communications Middleware
+ * 
+ * Copyright (c) 2008, Red Hat Middleware LLC or third-party contributors as
+ * indicated by the @author tags or express copyright attribution statements
+ * applied by the authors. All third-party contributions are distributed under
+ * license by Red Hat Middleware LLC.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.mobicents.diameter.dictionary;
 
+/**
+ * 
+ * AvpRepresentation.java
+ *
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a> 
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
+ * @version 1.0 
+ */
 public class AvpRepresentation
 {
   private String name;
   private String description;
   private int code;
   private boolean mayEncrypt;
+  private boolean _protected;
+  private boolean _mandatory;
   private String ruleMandatory;
   private String ruleProtected;
   private String ruleVendorBit;
-  private String vendorId;
+  private long vendorId;
   private boolean constrained;
   private String type;
 
+  private final static String _DEFAULT_MANDATORY = "may";
+  private final static String _DEFAULT_PROTECTED = "may";
+  private final static String _DEFAULT_VENDOR = "mustnot";
+
   enum Rule { MUST, MAY, MUSTNOT, SHOULDNOT };
-  
-  public AvpRepresentation( String name, String description, int code, boolean mayEncrypt, String ruleMandatory, String ruleProtected, String ruleVendorBit, String vendorId, boolean constrained, String type )
+
+  public AvpRepresentation( String name, String description, int code, boolean mayEncrypt, String ruleMandatory, String ruleProtected, String ruleVendorBit, long vendorId, boolean constrained, String type )
   {
     this.name = name;
     this.description = description;
@@ -24,9 +61,19 @@ public class AvpRepresentation
     this.ruleMandatory = ruleMandatory;
     this.ruleProtected = ruleProtected;
     this.ruleVendorBit = ruleVendorBit;
+    
+    if(this.ruleMandatory==null|| this.ruleMandatory.equals(""))
+      this.ruleMandatory=_DEFAULT_MANDATORY;
+    if(this.ruleProtected==null|| this.ruleProtected.equals(""))
+      this.ruleProtected=_DEFAULT_PROTECTED;
+    if(this.ruleVendorBit==null|| this.ruleVendorBit.equals(""))
+      this.ruleVendorBit=_DEFAULT_VENDOR;
+
     this.vendorId = vendorId;
     this.constrained = constrained;
     this.type = type;
+    this._mandatory=this.ruleMandatory.equals("must");
+    this._protected=this.ruleProtected.equals("must");
   }
 
   public String getName()
@@ -64,7 +111,7 @@ public class AvpRepresentation
     return ruleVendorBit;
   }
 
-  public String getVendorId()
+  public long getVendorId()
   {
     return vendorId;
   }
@@ -79,12 +126,20 @@ public class AvpRepresentation
     return type;
   }
 
+  public boolean isProtected()
+  {
+    return _protected;
+  }
 
-public String toString() {
-	String value="AVP: "+name+", CODE: "+code+", VID: "+vendorId+", RM: "+ruleMandatory+", RP: "+ruleProtected+", RV: "+ruleVendorBit+", DESC: "+description;
-	return value;
-}
-  
-  
-  
+  public boolean isMandatory()
+  {
+    return _mandatory;
+  }
+
+  public String toString()
+  {
+    String value="AVP: "+name+", CODE: "+code+", VID: "+vendorId+", RM: "+ruleMandatory+", RP: "+ruleProtected+", RV: "+ruleVendorBit+", DESC: "+description;
+    return value;
+  }
+
 }
