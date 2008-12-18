3d5ab34e971a0275b462cc0afb5f9c4b0734f54c
Alexandre Mendonça <ammendonca>
2008-12-18 18:36:11 +0000

    Fixing Sh-Common events for new base changes.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@4055 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/sh-common/events/src/main/java/org/mobicents/slee/resource/diameter/sh/client/events/avp/SupportedFeaturesAvpImpl.java
+++ resources/sh-common/events/src/main/java/org/mobicents/slee/resource/diameter/sh/client/events/avp/SupportedFeaturesAvpImpl.java
@@ -5,63 +5,67 @@ import net.java.slee.resource.diameter.sh.client.events.avp.SupportedFeaturesAvp
 
 import org.jdiameter.api.Avp;
 import org.mobicents.slee.resource.diameter.base.events.avp.GroupedAvpImpl;
-import org.mobicents.diameter.dictionary.AvpDictionary;
-import org.mobicents.diameter.dictionary.AvpRepresentation;
 
-
+/**
+ * 
+ * SupportedFeaturesAvpImpl.java
+ *
+ * <br>Super project:  mobicents
+ * <br>5:34:33 PM Dec 18, 2008 
+ * <br>
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a> 
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a> 
+ * @author Erick Svenson
+ */
 public class SupportedFeaturesAvpImpl extends GroupedAvpImpl implements SupportedFeaturesAvp {
 
-	public SupportedFeaturesAvpImpl(int code, long vendorId, int mnd, int prt, byte[] value) {
-		super(code, vendorId, mnd, prt, value);
+  public SupportedFeaturesAvpImpl(int code, long vendorId, int mnd, int prt, byte[] value)
+  {
+    super(code, vendorId, mnd, prt, value);
+  }
 
-	}
+  public long getFeatureList()
+  {
+    return super.getAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST);
+  }
 
-	public long getFeatureList() {
-		return super.getAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST);
-	}
+  public long getFeatureListId()
+  {
+    return super.getAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST_ID);
+  }
 
-	public long getFeatureListId() {
-		return super.getAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST_ID);
-	}
+  public boolean hasFeatureList()
+  {
+    return super.avpSet.getAvp(DiameterShAvpCodes.FEATURE_LIST) != null;
+  }
 
-	public boolean hasFeatureList() {
-		Avp rawAvp = super.avpSet.getAvp(DiameterShAvpCodes.FEATURE_LIST);
-		return rawAvp != null;
-	}
+  public boolean hasFeatureListId()
+  {
+    return super.avpSet.getAvp(DiameterShAvpCodes.FEATURE_LIST_ID) != null;
+  }
 
-	public boolean hasFeatureListId() {
+  public void setFeatureList(long featureList)
+  {
+    super.setAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST, featureList, true);
+  }
 
-		Avp rawAvp = super.avpSet.getAvp(DiameterShAvpCodes.FEATURE_LIST_ID);
-		return rawAvp != null;
-	}
+  public void setFeatureListId(long featureListId)
+  {
+    super.setAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST_ID, featureListId, true);
+  }
 
-	public void setFeatureList(long featureList) {
-		AvpRepresentation avpRep = AvpDictionary.INSTANCE.getAvp(DiameterShAvpCodes.FEATURE_LIST,10415);
+  public boolean hasVendorId()
+  {
+    return super.avpSet.getAvp(Avp.VENDOR_ID) != null;
+  }
 
-		int mandatoryAvp = avpRep.getRuleMandatory().equals("mustnot") || avpRep.getRuleMandatory().equals("shouldnot") ? 0 : 1;
-		
-		super.setAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST, featureList, mandatoryAvp==1, true);
-	}
+  public void setVendorId(long vendorId)
+  {
+    if (hasVendorId())
+    {
+      throw new IllegalStateException("Unable to set Vendor-Id AVP, it is already present in this message.");
+    }
 
-	public void setFeatureListId(long featureListId) {
-		AvpRepresentation avpRep = AvpDictionary.INSTANCE.getAvp(DiameterShAvpCodes.FEATURE_LIST_ID,10415);
-
-		int mandatoryAvp = avpRep.getRuleMandatory().equals("mustnot") || avpRep.getRuleMandatory().equals("shouldnot") ? 0 : 1;
-		super.setAvpAsUInt32(DiameterShAvpCodes.FEATURE_LIST_ID, featureListId, mandatoryAvp==1, true);
-	}
-
-	public boolean hasVendorId() {
-		Avp rawAvp = super.avpSet.getAvp(Avp.VENDOR_ID);
-		return rawAvp != null;
-	}
-
-	public void setVendorId(long vendorId) {
-		if (hasVendorId())
-			throw new IllegalStateException("Cant set vendor Id again!!!!");
-		AvpRepresentation avpRep = AvpDictionary.INSTANCE.getAvp(Avp.VENDOR_ID);
-
-		int mandatoryAvp = avpRep.getRuleMandatory().equals("mustnot") || avpRep.getRuleMandatory().equals("shouldnot") ? 0 : 1;
-		super.setAvpAsUInt32(Avp.VENDOR_ID, vendorId, mandatoryAvp==1, true);
-
-	}
+    super.setAvpAsUInt32(Avp.VENDOR_ID, vendorId, true);
+  }
 }
