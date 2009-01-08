3c059c95b3f36fd32e0e5b6b4d34cfa54d53283e
Alexandre Mendonça <ammendonca>
2009-01-08 16:35:59 +0000

    Cleaning imports. Was causing dep on CCA.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@4238 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/sh-server/ratype/src/main/java/net/java/slee/resource/diameter/sh/server/handlers/ShServerSessionListener.java
+++ resources/sh-server/ratype/src/main/java/net/java/slee/resource/diameter/sh/server/handlers/ShServerSessionListener.java
@@ -4,34 +4,32 @@ import javax.slee.resource.ActivityHandle;
 
 import org.jdiameter.api.Answer;
 import org.jdiameter.api.Request;
-import org.jdiameter.api.cca.ServerCCASession;
 import org.jdiameter.api.sh.ServerShSession;
 
 public interface ShServerSessionListener {
-	public void sessionDestroyed(String sessionId,ServerShSession session);
-	/**
-	   * Listener for Server Session creation.
-	   */
-	  public void sessionCreated(ServerShSession session, boolean isSubscription);
-	  
-	  
-	  
-	  //More like cca than client :/
-	  /**
-	   * Helper method to fire events to SLEE.
-	   * @param sessionId the id of the session for this event
-	   * @param name the event name (without suffix Request/Answer)
-	   * @param request the request object (if it is a request)
-	   * @param answer the answer object (if it is a answer)
-	   */
-	  public void fireEvent(String sessionId, String name, Request request, Answer answer);
 
-	  /**
-	   * Helper method to fire events to SLEE.
-	   * @param handle the activity handle where to fire it
-	   * @param name the event name (without suffix Request/Answer)
-	   * @param request the request object (if it is a request)
-	   * @param answer the answer object (if it is a answer)
-	   */
-	  public void fireEvent(ActivityHandle handle, String name, Request request, Answer answer);
+  public void sessionDestroyed(String sessionId,ServerShSession session);
+
+  /**
+	 * Listener for Server Session creation.
+	 */
+	public void sessionCreated(ServerShSession session, boolean isSubscription);
+
+  /**
+   * Helper method to fire events to SLEE.
+   * @param sessionId the id of the session for this event
+   * @param name the event name (without suffix Request/Answer)
+   * @param request the request object (if it is a request)
+   * @param answer the answer object (if it is a answer)
+   */
+  public void fireEvent(String sessionId, String name, Request request, Answer answer);
+
+  /**
+   * Helper method to fire events to SLEE.
+   * @param handle the activity handle where to fire it
+   * @param name the event name (without suffix Request/Answer)
+   * @param request the request object (if it is a request)
+   * @param answer the answer object (if it is a answer)
+   */
+  public void fireEvent(ActivityHandle handle, String name, Request request, Answer answer);
 }
