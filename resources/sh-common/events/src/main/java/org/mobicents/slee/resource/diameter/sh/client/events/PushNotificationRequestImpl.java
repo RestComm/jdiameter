85642d60750d3cc95aa55d211a0b62ff5c972055
Bartosz Baranowski <baranowb>
2009-05-22 07:25:41 +0000

    change:  - make  setUserIdentity work correctly.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@6088 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/sh-common/events/src/main/java/org/mobicents/slee/resource/diameter/sh/client/events/PushNotificationRequestImpl.java
+++ resources/sh-common/events/src/main/java/org/mobicents/slee/resource/diameter/sh/client/events/PushNotificationRequestImpl.java
@@ -8,71 +8,74 @@ import org.apache.log4j.Logger;
 import org.jdiameter.api.Avp;
 import org.jdiameter.api.AvpDataException;
 import org.jdiameter.api.Message;
+import org.mobicents.diameter.dictionary.AvpDictionary;
+import org.mobicents.diameter.dictionary.AvpRepresentation;
 import org.mobicents.slee.resource.diameter.sh.client.events.avp.UserIdentityAvpImpl;
 
 public class PushNotificationRequestImpl extends DiameterShMessageImpl implements PushNotificationRequest {
 
-  private static transient Logger logger = Logger.getLogger(PushNotificationRequestImpl.class);
+	private static transient Logger logger = Logger.getLogger(PushNotificationRequestImpl.class);
 
-  public PushNotificationRequestImpl(Message msg)
-  {
-    super(msg);
-    msg.setRequest(true);
-    super.longMessageName = "Push-Notification-Request";
-    super.shortMessageName = "PNR";
-  }
+	public PushNotificationRequestImpl(Message msg) {
+		super(msg);
+		msg.setRequest(true);
+		super.longMessageName = "Push-Notification-Request";
+		super.shortMessageName = "PNR";
+	}
 
-  public UserIdentityAvp getUserIdentity()
-  {
-    if (!hasUserIdentity())
-    {
-      return null;
-    }
+	public UserIdentityAvp getUserIdentity() {
+		if (!hasUserIdentity()) {
+			return null;
+		}
 
-    Avp rawAvp = super.message.getAvps().getAvp(DiameterShAvpCodes.USER_IDENTITY);
+		Avp rawAvp = super.message.getAvps().getAvp(DiameterShAvpCodes.USER_IDENTITY);
 
-    try {
-      return new UserIdentityAvpImpl(rawAvp.getCode(), rawAvp.getVendorId(), rawAvp.isMandatory() ? 1 : 0, rawAvp.isEncrypted() ? 1 : 0, rawAvp.getRaw());
-    }
-    catch (AvpDataException e) {
-      logger.error( "Unable to decode User-Identity AVP contents.", e );
-    }
+		try {
+			return new UserIdentityAvpImpl(rawAvp.getCode(), rawAvp.getVendorId(), rawAvp.isMandatory() ? 1 : 0, rawAvp.isEncrypted() ? 1 : 0, rawAvp.getRaw());
+		} catch (AvpDataException e) {
+			logger.error("Unable to decode User-Identity AVP contents.", e);
+		}
 
-    return null;
-  }
+		return null;
+	}
 
-  public boolean hasUserIdentity()
-  {
-    return super.message.getAvps().getAvp(DiameterShAvpCodes.USER_IDENTITY) != null;
-  }
+	public boolean hasUserIdentity() {
+		return super.message.getAvps().getAvp(DiameterShAvpCodes.USER_IDENTITY) != null;
+	}
 
-  public void setUserIdentity(UserIdentityAvp userIdentity)
-  {
-    super.setAvpAsGroup(userIdentity.getCode(), 10415L, new UserIdentityAvp[] { userIdentity }, true, true);
-  }
 
-  public boolean hasUserData()
-  {
-    return super.message.getAvps().getAvp(DiameterShAvpCodes.USER_DATA) != null;
-  }
 
-  public String getUserData()
-  {
-    try
-    {
-      return hasUserData() ? new String(super.message.getAvps().getAvp(DiameterShAvpCodes.USER_DATA).getRaw()) : null;
-    }
-    catch ( AvpDataException e ) {
-      logger.error( "Unable to decode User-Data AVP contents.", e );
-    }
+	public void setUserIdentity(UserIdentityAvp userIdentity) {
+		if (hasUserIdentity()) {
+			throw new IllegalStateException("AVP User-Identity is already present in message and cannot be overwritten.");
+		} else {
+			AvpRepresentation avpRep = AvpDictionary.INSTANCE.getAvp(DiameterShAvpCodes.USER_IDENTITY, 10415L);
+			int mandatoryAvp = avpRep.getRuleMandatory().equals("mustnot") || avpRep.getRuleMandatory().equals("shouldnot") ? 0 : 1;
+			// int protectedAvp = avpRep.getRuleProtected().equals("must") ? 1 :
+			// 0;
 
-    return null;
-  }
+			// FIXME: Alexandre: Need to specify protected!
+			super.setAvpAsGroup(avpRep.getCode(), avpRep.getVendorId(), userIdentity.getExtensionAvps(), mandatoryAvp == 1, false);
+		}
+	}
 
-  public void setUserData(byte[] userData)
-  {
-    super.message.getAvps().removeAvp(DiameterShAvpCodes.USER_DATA);
-    super.message.getAvps().addAvp(DiameterShAvpCodes.USER_DATA, userData, 10415L, true, false);
-  }
+	public boolean hasUserData() {
+		return super.message.getAvps().getAvp(DiameterShAvpCodes.USER_DATA) != null;
+	}
+
+	public String getUserData() {
+		try {
+			return hasUserData() ? new String(super.message.getAvps().getAvp(DiameterShAvpCodes.USER_DATA).getRaw()) : null;
+		} catch (AvpDataException e) {
+			logger.error("Unable to decode User-Data AVP contents.", e);
+		}
+
+		return null;
+	}
+
+	public void setUserData(byte[] userData) {
+		super.message.getAvps().removeAvp(DiameterShAvpCodes.USER_DATA);
+		super.message.getAvps().addAvp(DiameterShAvpCodes.USER_DATA, userData, 10415L, true, false);
+	}
 
 }
