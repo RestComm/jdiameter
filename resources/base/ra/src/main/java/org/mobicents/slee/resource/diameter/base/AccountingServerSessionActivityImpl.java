03b9be5e4156f973c8b561df859f165a31f5d2ba
Bartosz Baranowski <baranowb>
2009-05-28 13:52:04 +0000

    change:  - add 193xxx as app id for 271 to xml file, so testsuite passes correctly.  - minor fix to session.
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@6206 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/base/ra/src/main/java/org/mobicents/slee/resource/diameter/base/AccountingServerSessionActivityImpl.java
+++ resources/base/ra/src/main/java/org/mobicents/slee/resource/diameter/base/AccountingServerSessionActivityImpl.java
@@ -29,97 +29,89 @@ import org.mobicents.slee.resource.diameter.base.events.DiameterMessageImpl;
 /**
  * 
  * AccountingServerSessionActivityImpl.java
- *
+ * 
  * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a>
  * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
  */
-public class AccountingServerSessionActivityImpl extends AccountingSessionActivityImpl
-		implements AccountingServerSessionActivity {
+public class AccountingServerSessionActivityImpl extends AccountingSessionActivityImpl implements AccountingServerSessionActivity {
 
 	protected ServerAccSession serverSession = null;
-	
+
 	// These are default values, should be overriden by stack.
 	protected String originHost = "aaa://127.0.0.1:3868";
 	protected String originRealm = "mobicents.org";
-	
+
 	boolean destroyAfterSending = false;
-	
-	public AccountingServerSessionActivityImpl(DiameterMessageFactoryImpl messageFactory, DiameterAvpFactoryImpl avpFactory, ServerAccSession serverSession, long timeout, DiameterIdentity destinationHost, DiameterIdentity destinationRealm,SleeEndpoint endpoint,
-			Stack stack) 
-	{
-		super(messageFactory, avpFactory, null, (EventListener<Request, Answer>) serverSession, timeout, destinationHost, destinationRealm,endpoint);
+
+	public AccountingServerSessionActivityImpl(DiameterMessageFactoryImpl messageFactory, DiameterAvpFactoryImpl avpFactory, ServerAccSession serverSession, long timeout,
+			DiameterIdentity destinationHost, DiameterIdentity destinationRealm, SleeEndpoint endpoint, Stack stack) {
+		super(messageFactory, avpFactory, null, (EventListener<Request, Answer>) serverSession, timeout, destinationHost, destinationRealm, endpoint);
 
 		this.serverSession = serverSession;
-		//this.serverSession.addStateChangeNotification(this);
-		
+		// this.serverSession.addStateChangeNotification(this);
+
 		super.setCurrentWorkingSession(this.serverSession.getSessions().get(0));
-		this.state=AccountingSessionState.Idle;
-    this.originHost = stack.getMetaData().getLocalPeer().getUri().toString();
-    this.originRealm = stack.getMetaData().getLocalPeer().getRealmName();
+		this.state = AccountingSessionState.Idle;
+		this.originHost = stack.getMetaData().getLocalPeer().getUri().toString();
+		this.originRealm = stack.getMetaData().getLocalPeer().getRealmName();
 	}
 
-	public AccountingAnswer createAccountAnswer(AccountingRequest request)
-	{
-    try
-    {
-      // Get the impl
-      DiameterMessageImpl implRequest = (DiameterMessageImpl)request;
-      
-      // Get raw message from impl
-      Message rawMessage = implRequest.getGenericData();
-      
-      // Extract interesting AVPs
-      DiameterAvp accRecordNumber = avpFactory.createAvp(Avp.ACC_RECORD_NUMBER, rawMessage.getAvps().getAvp(Avp.ACC_RECORD_NUMBER).getRaw());
-      DiameterAvp accRecordType = avpFactory.createAvp(Avp.ACC_RECORD_TYPE, rawMessage.getAvps().getAvp(Avp.ACC_RECORD_TYPE).getRaw());
-      
-      DiameterAvp originHost = avpFactory.createAvp(Avp.ORIGIN_HOST, this.originHost.getBytes());
-      DiameterAvp originRealm = avpFactory.createAvp(Avp.ORIGIN_REALM, this.originRealm.getBytes());
-      
-      DiameterAvp sessionId = avpFactory.createAvp(Avp.SESSION_ID, rawMessage.getAvps().getAvp(Avp.SESSION_ID).getRaw());
-      
-      DiameterMessageImpl answer = (DiameterMessageImpl) messageFactory.createMessage( implRequest.getHeader(), new DiameterAvp[]{accRecordNumber, accRecordType, originHost, originRealm, sessionId} );
-      
-      // RFC3588, Page 119-120
-      // One of Acct-Application-Id and Vendor-Specific-Application-Id AVPs
-      // MUST be present.  If the Vendor-Specific-Application-Id grouped AVP
-      // is present, it must have an Acct-Application-Id inside.
+	public AccountingAnswer createAccountAnswer(AccountingRequest request) {
+		try {
+			// Get the impl
+			DiameterMessageImpl implRequest = (DiameterMessageImpl) request;
 
-      if(request.hasAcctApplicationId())
-      {
-        answer.addAvp( avpFactory.createAvp(Avp.ACCT_APPLICATION_ID, request.getAcctApplicationId()) );
-      }
-      else
-      {
-        // We should have an Vendor-Specific-Application-Id grouped AVP
-        answer.addAvp( request.getVendorSpecificApplicationId() );
-      }
-      
-      // Get the raw Answer
-      Message rawAnswer = answer.getGenericData();
+			// Get raw message from impl
+			Message rawMessage = implRequest.getGenericData();
 
-      // This is an answer.
-      rawAnswer.setRequest(false);
+			// Extract interesting AVPs
+			DiameterAvp accRecordNumber = avpFactory.createAvp(Avp.ACC_RECORD_NUMBER, rawMessage.getAvps().getAvp(Avp.ACC_RECORD_NUMBER).getRaw());
+			DiameterAvp accRecordType = avpFactory.createAvp(Avp.ACC_RECORD_TYPE, rawMessage.getAvps().getAvp(Avp.ACC_RECORD_TYPE).getRaw());
 
-      return new AccountingAnswerImpl( rawAnswer );
-    }
-    catch ( Exception e )
-    {
-      logger.error( "", e );
-    }
+			DiameterAvp originHost = avpFactory.createAvp(Avp.ORIGIN_HOST, this.originHost.getBytes());
+			DiameterAvp originRealm = avpFactory.createAvp(Avp.ORIGIN_REALM, this.originRealm.getBytes());
 
-    return null;
-  }
+			DiameterAvp sessionId = avpFactory.createAvp(Avp.SESSION_ID, rawMessage.getAvps().getAvp(Avp.SESSION_ID).getRaw());
 
-	
-	public AccountingAnswer createAccountAnswer(AccountingRequest request, int resultCode)
-	{
-	  AccountingAnswer answer = this.createAccountAnswer( request );
-	  
-	  answer.setResultCode( resultCode );
-	  
-	  return answer;
+			DiameterMessageImpl answer = (DiameterMessageImpl) messageFactory.createMessage(implRequest.getHeader(), new DiameterAvp[] { accRecordNumber, accRecordType, originHost, originRealm,
+					sessionId });
+
+			// RFC3588, Page 119-120
+			// One of Acct-Application-Id and Vendor-Specific-Application-Id
+			// AVPs
+			// MUST be present. If the Vendor-Specific-Application-Id grouped
+			// AVP
+			// is present, it must have an Acct-Application-Id inside.
+
+			if (request.hasAcctApplicationId()) {
+				answer.addAvp(avpFactory.createAvp(Avp.ACCT_APPLICATION_ID, request.getAcctApplicationId()));
+			} else {
+				// We should have an Vendor-Specific-Application-Id grouped AVP
+				answer.addAvp(request.getVendorSpecificApplicationId());
+			}
+
+			// Get the raw Answer
+			Message rawAnswer = answer.getGenericData();
+
+			// This is an answer.
+			rawAnswer.setRequest(false);
+
+			return new AccountingAnswerImpl(rawAnswer);
+		} catch (Exception e) {
+			logger.error("", e);
+		}
+
+		return null;
 	}
-	
+
+	public AccountingAnswer createAccountAnswer(AccountingRequest request, int resultCode) {
+		AccountingAnswer answer = this.createAccountAnswer(request);
+
+		answer.setResultCode(resultCode);
+
+		return answer;
+	}
+
 	public void sendAccountAnswer(AccountingAnswer answer) throws IOException {
 		// FIXME: baranowb - add setting of proper session
 		// super.sendMessage(answer);
@@ -170,17 +162,14 @@ public class AccountingServerSessionActivityImpl extends AccountingSessionActivi
 	}
 
 	public void stateChanged(Enum oldState, Enum newState) {
-		
-		if(newState==ServerAccSessionState.IDLE)
-		{
-			super.state=AccountingSessionState.Idle;
+
+		if (newState == ServerAccSessionState.IDLE) {
+			super.state = AccountingSessionState.Idle;
 			destroyAfterSending = true;
+		} else {
+			super.state = AccountingSessionState.Open;
 		}
-		else
-		{
-			super.state=AccountingSessionState.Open;
-		}
-		
+
 	}
 
 }
