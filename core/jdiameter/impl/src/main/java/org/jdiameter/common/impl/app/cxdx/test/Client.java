3a8f0a1714841f402481ff7eb161a83f3a34cfd7
Alexandre Mendonça <ammendonca>
2009-12-15 09:42:23 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@9004 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/jdiameter/impl/src/main/java/org/jdiameter/common/impl/app/cxdx/test/Client.java
+++ core/jdiameter/impl/src/main/java/org/jdiameter/common/impl/app/cxdx/test/Client.java
@@ -153,7 +153,7 @@ public class Client extends SessionFactoryCXDX implements NetworkReqListener, Ev
 	 * org.jdiameter.api.cxdx.events.JPushProfileRequest,
 	 * org.jdiameter.api.cxdx.events.JPushProfileAnswer)
 	 */
-	public void doPushProfileRequest(ClientCxDxSession session, JPushProfileRequest request, JPushProfileAnswer answer) throws InternalException, IllegalDiameterStateException, RouteException,
+	public void doPushProfileRequest(ClientCxDxSession session, JPushProfileRequest request) throws InternalException, IllegalDiameterStateException, RouteException,
 			OverloadException {
 		System.out.println("--[" + this.getClass().getSimpleName() + "]-- GOT doPushProfileRequest");
 
@@ -168,7 +168,7 @@ public class Client extends SessionFactoryCXDX implements NetworkReqListener, Ev
 	 * org.jdiameter.api.cxdx.events.JRegistrationTerminationRequest,
 	 * org.jdiameter.api.cxdx.events.JRegistrationTerminationAnswer)
 	 */
-	public void doRegistrationTerminationRequest(ClientCxDxSession session, JRegistrationTerminationRequest request, JRegistrationTerminationAnswer answer) throws InternalException,
+	public void doRegistrationTerminationRequest(ClientCxDxSession session, JRegistrationTerminationRequest request) throws InternalException,
 			IllegalDiameterStateException, RouteException, OverloadException {
 		System.out.println("--[" + this.getClass().getSimpleName() + "]-- GOT doRegistrationTerminationRequest");
 
@@ -213,7 +213,7 @@ public class Client extends SessionFactoryCXDX implements NetworkReqListener, Ev
 	 * org.jdiameter.api.cxdx.events.JLocationInfoRequest,
 	 * org.jdiameter.api.cxdx.events.JLocationInfoAnswer)
 	 */
-	public void doLocationInformationRequest(ServerCxDxSession session, JLocationInfoRequest request, JLocationInfoAnswer answer) throws InternalException, IllegalDiameterStateException,
+	public void doLocationInformationRequest(ServerCxDxSession session, JLocationInfoRequest request) throws InternalException, IllegalDiameterStateException,
 			RouteException, OverloadException {
 		System.out.println("--[" + this.getClass().getSimpleName() + "]-- GOT doLocationInformationRequest");
 
@@ -228,7 +228,7 @@ public class Client extends SessionFactoryCXDX implements NetworkReqListener, Ev
 	 * org.jdiameter.api.cxdx.events.JMultimediaAuthRequest,
 	 * org.jdiameter.api.cxdx.events.JMultimediaAuthAnswer)
 	 */
-	public void doMultimediaAuthRequest(ServerCxDxSession session, JMultimediaAuthRequest request, JMultimediaAuthAnswer answer) throws InternalException, IllegalDiameterStateException,
+	public void doMultimediaAuthRequest(ServerCxDxSession session, JMultimediaAuthRequest request) throws InternalException, IllegalDiameterStateException,
 			RouteException, OverloadException {
 		System.out.println("--[" + this.getClass().getSimpleName() + "]-- GOT doMultimediaAuthRequest");
 
@@ -272,7 +272,7 @@ public class Client extends SessionFactoryCXDX implements NetworkReqListener, Ev
 	 * org.jdiameter.api.cxdx.events.JServerAssignmentRequest,
 	 * org.jdiameter.api.cxdx.events.JServerAssignmentAnswer)
 	 */
-	public void doServerAssignmentRequest(ServerCxDxSession session, JServerAssignmentRequest request, JServerAssignmentAnswer answer) throws InternalException, IllegalDiameterStateException,
+	public void doServerAssignmentRequest(ServerCxDxSession session, JServerAssignmentRequest request) throws InternalException, IllegalDiameterStateException,
 			RouteException, OverloadException {
 		System.out.println("--[" + this.getClass().getSimpleName() + "]-- GOT doServerAssignmentRequest");
 
@@ -287,7 +287,7 @@ public class Client extends SessionFactoryCXDX implements NetworkReqListener, Ev
 	 * org.jdiameter.api.cxdx.events.JUserAuthorizationRequest,
 	 * org.jdiameter.api.cxdx.events.JUserAuthorizationAnswer)
 	 */
-	public void doUserAuthorizationRequest(ServerCxDxSession session, JUserAuthorizationRequest request, JUserAuthorizationAnswer answer) throws InternalException, IllegalDiameterStateException,
+	public void doUserAuthorizationRequest(ServerCxDxSession session, JUserAuthorizationRequest request) throws InternalException, IllegalDiameterStateException,
 			RouteException, OverloadException {
 		System.out.println("--[" + this.getClass().getSimpleName() + "]-- GOT doUserAuthorizationRequest");
 
