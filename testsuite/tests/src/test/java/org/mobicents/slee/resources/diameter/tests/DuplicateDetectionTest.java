8f4c172e535933c59500616e6c089e2ab1c803ef
Alexandre Mendonça <ammendonca>
2010-10-26 17:46:44 +0000

    Testsuite for Diameter duplicates
    Update Issue 1946
    Added Testsuite for Diameter duplicates
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@14923 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ testsuite/tests/src/test/java/org/mobicents/slee/resources/diameter/tests/DuplicateDetectionTest.java
@@ -0,0 +1,40 @@
+package org.mobicents.slee.resources.diameter.tests;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.mobicents.slee.resources.diameter.tests.framework.TestingFramework;
+
+@RunWith(Parameterized.class)
+public class DuplicateDetectionTest {
+
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {"scenarios/duplicate-detection-test.xml"},
+        {"scenarios/duplicate-detection-test-diff-e2e.xml"},
+        {"scenarios/duplicate-detection-test-diff-origin-host.xml"},
+        {"scenarios/duplicate-detection-test-diff-origin-host-e2e.xml"},
+        {"scenarios/duplicate-detection-test-rtr-flag-unset.xml"},
+        {"scenarios/duplicate-detection-test-rtr-sent-first.xml"},
+        {"scenarios/duplicate-detection-test-three-times.xml"}
+    });
+  }
+
+  private String scenario;
+  
+  public DuplicateDetectionTest(String scenario) {
+    this.scenario = scenario;
+  }
+  
+  @Test
+  public void runTest() throws FileNotFoundException {
+    TestingFramework tF = new TestingFramework();
+    tF.executeTest(this.getClass().getClassLoader().getResourceAsStream(scenario));
+  }
+}
