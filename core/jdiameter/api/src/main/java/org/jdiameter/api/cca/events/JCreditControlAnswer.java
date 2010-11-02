e68b3be701dfd29d76f7f0ce43004f7c26ba849c
Alexandre Mendonça <ammendonca>
2010-11-02 17:17:24 +0000

    Improve sessions and messages with all relevant setter/getters - Stack CCA API
    Update Issue 1257
    Commited Stack CCA API changes
    
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@14971 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/jdiameter/api/src/main/java/org/jdiameter/api/cca/events/JCreditControlAnswer.java
+++ core/jdiameter/api/src/main/java/org/jdiameter/api/cca/events/JCreditControlAnswer.java
@@ -1,38 +1,98 @@
 package org.jdiameter.api.cca.events;
 
-import org.jdiameter.api.Avp;
+import org.jdiameter.api.AvpDataException;
 import org.jdiameter.api.app.AppAnswerEvent;
 
 /**
  * 
  * JCreditControlAnswer.java
- *
- * <br>Super project:  mobicents
- * <br>3:45:53 PM Dec 2, 2008 
+ * 
  * <br>
- * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a> 
- * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a> 
+ * Super project: mobicents <br>
+ * 3:45:53 PM Dec 2, 2008 <br>
+ * 
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a>
  * @author Erick Svenson
  */
 public interface JCreditControlAnswer extends AppAnswerEvent {
-	
-	public static final String _SHORT_NAME = "CCA";
-	public static final String _LONG_NAME = "Credit-Control-Answer";
-	
-	public static final int code = 272;
-	
-	boolean isCreditControlFailureHandlingAVPPresent();
-	
-	int getCredidControlFailureHandlingAVPValue();
-	
-	boolean isDirectDebitingFailureHandlingAVPPresent();
-	
-	int getDirectDebitingFailureHandlingAVPValue();
-	
-	boolean isRequestTypeAVPPresent();
-	
-	int getRequestTypeAVPValue();
-	
-	Avp getValidityTimeAvp();
+
+  public static final String _SHORT_NAME = "CCA";
+  public static final String _LONG_NAME = "Credit-Control-Answer";
+
+  public static final int code = 272;
+
+  boolean hasAcctMultiSessionId();
+
+  String getAcctMultiSessionId() throws AvpDataException;
+
+  void setAcctMultiSessionId(String val);
+
+  long getAuthApplicationId() throws AvpDataException;
+
+  boolean hasAuthApplicationId();
+
+  void setAuthApplicationId(long val);
+
+  long getCCRequestNumber() throws AvpDataException;
+
+  boolean hasCCRequestNumber();
+
+  void setCCRequestNumber(long val);
+
+  int getCCRequestType() throws AvpDataException;
+
+  boolean hasCCRequestType();
+
+  void setCCRequestType(int val);
+
+  long getCCSubSessionId() throws AvpDataException;
+
+  boolean hasCCSubSessionId();
+
+  void setCCSubSessionId(long val);
+
+  int getCheckBalanceResult() throws AvpDataException;
+
+  boolean hasCheckBalanceResult();
+
+  void setCheckBalanceResult(int val);
+
+  int getCCFailureHandling() throws AvpDataException;
+
+  boolean hasCCFailureHandling();
+
+  void setCCFailureHandling(int val);
+
+  int getDDFailureHandling() throws AvpDataException;
+
+  boolean hasDDFailureHandling();
+
+  void setDDFailureHandling(int val);
+
+  boolean hasEventTimestamp();
+
+  String getEventTimestamp() throws AvpDataException;
+
+  void setEventTimestamp(String val);
+
+  long getOriginStateId() throws AvpDataException;
+
+  boolean hasOriginStateId();
+
+  void setOriginStateId(long val);
+
+  boolean hasUserName();
+
+  String getUserName() throws AvpDataException;
+
+  void setUserName(String val);
+
+  //unsigned32
+  boolean hasValidityTime();
+
+  long getValidityTime() throws AvpDataException;
+
+  void setValidityTime(long val);
 
 }
--- core/jdiameter/api/src/main/java/org/jdiameter/api/cca/events/JCreditControlRequest.java
+++ core/jdiameter/api/src/main/java/org/jdiameter/api/cca/events/JCreditControlRequest.java
@@ -1,30 +1,109 @@
 package org.jdiameter.api.cca.events;
 
+import org.jdiameter.api.AvpDataException;
 import org.jdiameter.api.app.AppRequestEvent;
 
 /**
  * 
  * JCreditControlRequest.java
- *
- * <br>Super project:  mobicents
- * <br>3:46:49 PM Dec 2, 2008 
+ * 
  * <br>
- * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a> 
- * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a> 
+ * Super project: mobicents <br>
+ * 3:46:49 PM Dec 2, 2008 <br>
+ * 
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a>
  * @author Erick Svenson
  */
 public interface JCreditControlRequest extends AppRequestEvent {
-  
-	public static final String _SHORT_NAME = "CCR";
-	public static final String _LONG_NAME = "Credit-Control-Request";
-	
-	public static final int code = 272;
-	
-	boolean isRequestedActionAVPPresent();
-	
-	int getRequestedActionAVPValue();
-	
-	boolean isRequestTypeAVPPresent();
-	
-	int getRequestTypeAVPValue();
+
+  public static final String _SHORT_NAME = "CCR";
+  public static final String _LONG_NAME = "Credit-Control-Request";
+
+  public static final int code = 272;
+
+  boolean hasAcctMultiSessionId();
+
+  String getAcctMultiSessionId() throws AvpDataException;
+
+  void setAcctMultiSessionId(String val);
+
+  long getAuthApplicationId() throws AvpDataException;
+
+  boolean hasAuthApplicationId();
+
+  void setAuthApplicationId(long val);
+
+  String getCCCorrelationId() throws AvpDataException;
+
+  boolean hasCCCorrelationId();
+
+  void setCCCorrelationId(String val);
+
+  long getCCRequestNumber() throws AvpDataException;
+
+  boolean hasCCRequestNumber();
+
+  void setCCRequestNumber(long val);
+
+  int getCCRequestType() throws AvpDataException;
+
+  boolean hasCCRequestType();
+
+  void setCCRequestType(int val);
+
+  long getCCSubSessionId() throws AvpDataException;
+
+  boolean hasCCSubSessionId();
+
+  void setCCSubSessionId(long val);
+
+  boolean hasEventTimestamp();
+
+  String getEventTimestamp() throws AvpDataException;
+
+  void setEventTimestamp(String val);
+
+  long getOriginStateId() throws AvpDataException;
+
+  boolean hasOriginStateId();
+
+  void setOriginStateId(long val);
+
+  int getMultiServicesIndicator() throws AvpDataException;
+
+  boolean hasMultiServicesIndicator();
+
+  void setMultiServicesIndicator(int val);
+
+  int getRequestedAction() throws AvpDataException;
+
+  boolean hasRequestedAction();
+
+  void setRequestedAction(int val);
+
+  long getServiceId() throws AvpDataException;
+
+  boolean hasServiceId();
+
+  void setServiceId(long val);
+
+  String getServiceContextId() throws AvpDataException;
+
+  boolean hasServiceContextId();
+
+  void setServiceContextId(String val);
+
+  boolean hasTerminationCause();
+
+  int getTerminationCause() throws AvpDataException;
+
+  void setTerminationCause(int val);
+
+  boolean hasUserName();
+
+  String getUserName() throws AvpDataException;
+
+  void setUserName(String val);
+
 }
