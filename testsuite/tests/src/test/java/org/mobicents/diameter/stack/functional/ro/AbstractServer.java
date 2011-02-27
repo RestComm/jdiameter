d7cd41673820bc25d7c32c03aecac4e1b3582503
Alexandre Mendonça <ammendonca>
2011-02-27 02:46:24 +0000

    Diameter Ro Functional Testsuite
    Update Issue 2388
    Added testsuite for Diameter Ro
    
    
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@17029 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ testsuite/tests/src/test/java/org/mobicents/diameter/stack/functional/ro/AbstractServer.java
@@ -0,0 +1,141 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing
+ * of individual contributors.
+ * 
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License, v. 2.0.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License,
+ * v. 2.0 along with this distribution; if not, write to the Free 
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+package org.mobicents.diameter.stack.functional.ro;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.jdiameter.api.ApplicationId;
+import org.jdiameter.api.IllegalDiameterStateException;
+import org.jdiameter.api.InternalException;
+import org.jdiameter.api.Mode;
+import org.jdiameter.api.ro.ClientRoSession;
+import org.jdiameter.api.ro.ServerRoSession;
+import org.jdiameter.api.ro.ServerRoSessionListener;
+import org.jdiameter.client.api.ISessionFactory;
+import org.jdiameter.common.api.app.ro.IServerRoSessionContext;
+import org.jdiameter.common.api.app.ro.ServerRoSessionState;
+import org.jdiameter.common.impl.app.ro.RoSessionFactoryImpl;
+import org.mobicents.diameter.stack.functional.StateChange;
+import org.mobicents.diameter.stack.functional.TBase;
+
+/**
+ * 
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a>
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
+ */
+public abstract class AbstractServer extends TBase implements ServerRoSessionListener, IServerRoSessionContext {
+
+  // NOTE: implementing NetworkReqListener since its required for stack to
+  // know we support it... ech.
+
+  protected static final int CC_REQUEST_TYPE_INITIAL = 1;
+  protected static final int CC_REQUEST_TYPE_INTERIM = 2;
+  protected static final int CC_REQUEST_TYPE_TERMINATE = 3;
+  protected static final int CC_REQUEST_TYPE_EVENT = 4;
+
+  protected ServerRoSession serverRoSession;
+  protected int ccRequestNumber = 0;
+
+  protected List<StateChange<ServerRoSessionState>> stateChanges = new ArrayList<StateChange<ServerRoSessionState>>(); // state changes
+
+  public void init(InputStream configStream, String clientID) throws Exception {
+    try {
+      super.init(configStream, clientID, ApplicationId.createByAuthAppId(0, 4));
+      RoSessionFactoryImpl creditControlSessionFactory = new RoSessionFactoryImpl(this.sessionFactory);
+      ((ISessionFactory) sessionFactory).registerAppFacory(ServerRoSession.class, creditControlSessionFactory);
+      ((ISessionFactory) sessionFactory).registerAppFacory(ClientRoSession.class, creditControlSessionFactory);
+
+      creditControlSessionFactory.setStateListener(this);
+      creditControlSessionFactory.setServerSessionListener(this);
+      creditControlSessionFactory.setServerContextListener(this);
+    }
+    finally {
+      try {
+        configStream.close();
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  // ----------- delegate methods so
+
+  public void start() throws IllegalDiameterStateException, InternalException {
+    stack.start();
+  }
+
+  public void start(Mode mode, long timeOut, TimeUnit timeUnit) throws IllegalDiameterStateException, InternalException {
+    stack.start(mode, timeOut, timeUnit);
+  }
+
+  public void stop(long timeOut, TimeUnit timeUnit) throws IllegalDiameterStateException, InternalException {
+    stack.stop(timeOut, timeUnit);
+  }
+
+  public void stop() {
+    stack.stop();
+  }
+
+  // ----------- conf parts
+
+  public void sessionSupervisionTimerExpired(ServerRoSession session) {
+    // NOP
+  }
+
+  public void sessionSupervisionTimerStarted(ServerRoSession session, ScheduledFuture future) {
+    // NOP
+  }
+
+  public void sessionSupervisionTimerReStarted(ServerRoSession session, ScheduledFuture future) {
+    // NOP
+  }
+
+  public void sessionSupervisionTimerStopped(ServerRoSession session, ScheduledFuture future) {
+    // NOP
+  }
+
+  public long getDefaultValidityTime() {
+    return 120;
+  }
+
+  public String getSessionId() {
+    return this.serverRoSession.getSessionId();
+  }
+
+  public void fetchSession(String sessionId) throws InternalException {
+    this.serverRoSession = stack.getSession(sessionId, ServerRoSession.class);
+  }
+
+  public ServerRoSession getSession() {
+    return this.serverRoSession;
+  }
+
+  public List<StateChange<ServerRoSessionState>> getStateChanges() {
+    return stateChanges;
+  }
+
+}
---
+++ testsuite/tests/src/test/java/org/mobicents/diameter/stack/functional/ro/base/Client.java
@@ -0,0 +1,214 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing
+ * of individual contributors.
+ * 
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License, v. 2.0.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License,
+ * v. 2.0 along with this distribution; if not, write to the Free 
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+package org.mobicents.diameter.stack.functional.ro.base;
+
+import org.jdiameter.api.IllegalDiameterStateException;
+import org.jdiameter.api.InternalException;
+import org.jdiameter.api.OverloadException;
+import org.jdiameter.api.RouteException;
+import org.jdiameter.api.app.AppAnswerEvent;
+import org.jdiameter.api.app.AppRequestEvent;
+import org.jdiameter.api.app.AppSession;
+import org.jdiameter.api.auth.events.ReAuthRequest;
+import org.jdiameter.api.ro.ClientRoSession;
+import org.jdiameter.api.ro.events.RoCreditControlAnswer;
+import org.jdiameter.api.ro.events.RoCreditControlRequest;
+import org.mobicents.diameter.stack.functional.Utils;
+import org.mobicents.diameter.stack.functional.ro.AbstractClient;
+
+/**
+ * Base implementation of Client
+ * 
+ * @author <a href="mailto:brainslog@gmail.com"> Alexandre Mendonca </a>
+ * @author <a href="mailto:baranowb@gmail.com"> Bartosz Baranowski </a>
+ */
+public class Client extends AbstractClient {
+
+  protected boolean sentINITIAL;
+  protected boolean sentINTERIM;
+  protected boolean sentTERMINATE;
+  protected boolean sentEVENT;
+  protected boolean receiveINITIAL;
+  protected boolean receiveINTERIM;
+  protected boolean receiveTERMINATE;
+  protected boolean receiveEVENT;
+
+  /**
+	 * 
+	 */
+  public Client() {
+  }
+
+  public void sendInitial() throws Exception {
+    RoCreditControlRequest initialRequest = super.createCCR(CC_REQUEST_TYPE_INITIAL, this.ccRequestNumber, super.clientRoSession);
+    this.ccRequestNumber++;
+    super.clientRoSession.sendCreditControlRequest(initialRequest);
+    Utils.printMessage(log, super.stack.getDictionary(), initialRequest.getMessage(), true);
+    this.sentINITIAL = true;
+  }
+
+  public void sendInterim() throws Exception {
+    if (!receiveINITIAL) {
+      throw new Exception();
+    }
+    RoCreditControlRequest interimRequest = super.createCCR(CC_REQUEST_TYPE_INTERIM, this.ccRequestNumber, super.clientRoSession);
+    this.ccRequestNumber++;
+    super.clientRoSession.sendCreditControlRequest(interimRequest);
+    Utils.printMessage(log, super.stack.getDictionary(), interimRequest.getMessage(), true);
+    this.sentINTERIM = true;
+  }
+
+  public void sendTermination() throws Exception {
+    if (!receiveINTERIM) {
+      throw new Exception();
+    }
+    RoCreditControlRequest terminateRequest = super.createCCR(CC_REQUEST_TYPE_TERMINATE, this.ccRequestNumber, super.clientRoSession);
+    this.ccRequestNumber++;
+    super.clientRoSession.sendCreditControlRequest(terminateRequest);
+    Utils.printMessage(log, super.stack.getDictionary(), terminateRequest.getMessage(), true);
+    this.sentTERMINATE = true;
+  }
+
+  public void sendEvent() throws Exception {
+    RoCreditControlRequest eventRequest = super.createCCR(CC_REQUEST_TYPE_TERMINATE, this.ccRequestNumber, super.clientRoSession);
+    this.ccRequestNumber++;
+    super.clientRoSession.sendCreditControlRequest(eventRequest);
+    Utils.printMessage(log, super.stack.getDictionary(), eventRequest.getMessage(), true);
+    this.sentEVENT = true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.jdiameter.api.cca.ClientCCASessionListener#doCreditControlAnswer( org.jdiameter.api.cca.ClientCCASession,
+   * org.jdiameter.api.cca.events.RoCreditControlRequest, org.jdiameter.api.cca.events.JCreditControlAnswer)
+   */
+  public void doCreditControlAnswer(ClientRoSession session, RoCreditControlRequest request, RoCreditControlAnswer answer) throws InternalException, IllegalDiameterStateException,
+      RouteException, OverloadException {
+    try {
+      Utils.printMessage(log, super.stack.getDictionary(), answer.getMessage(), false);
+      switch (answer.getRequestTypeAVPValue()) {
+      case CC_REQUEST_TYPE_INITIAL:
+        if (receiveINITIAL) {
+          fail("Received INITIAL more than once!", null);
+        }
+        receiveINITIAL = true;
+        break;
+
+      case CC_REQUEST_TYPE_INTERIM:
+        if (receiveINTERIM) {
+          fail("Received INTERIM more than once!", null);
+        }
+        receiveINTERIM = true;
+        break;
+
+      case CC_REQUEST_TYPE_TERMINATE:
+        if (receiveTERMINATE) {
+          fail("Received TERMINATE more than once!", null);
+        }
+        receiveTERMINATE = true;
+        break;
+
+      case CC_REQUEST_TYPE_EVENT:
+        if (receiveEVENT) {
+          fail("Received EVENT more than once!", null);
+        }
+        receiveEVENT = true;
+        break;
+
+      default:
+
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.jdiameter.api.cca.ClientCCASessionListener#doReAuthRequest(org.jdiameter .api.cca.ClientCCASession,
+   * org.jdiameter.api.auth.events.ReAuthRequest)
+   */
+  public void doReAuthRequest(ClientRoSession session, ReAuthRequest request) throws InternalException, IllegalDiameterStateException, RouteException, OverloadException {
+    fail("Received \"ReAuthRequest\" event, request[" + request + "], on session[" + session + "]", null);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.jdiameter.api.cca.ClientCCASessionListener#doOtherEvent(org.jdiameter .api.app.AppSession,
+   * org.jdiameter.api.app.AppRequestEvent, org.jdiameter.api.app.AppAnswerEvent)
+   */
+  public void doOtherEvent(AppSession session, AppRequestEvent request, AppAnswerEvent answer) throws InternalException, IllegalDiameterStateException, RouteException,
+      OverloadException {
+    fail("Received \"Other\" event, request[" + request + "], answer[" + answer + "], on session[" + session + "]", null);
+  }
+
+  // ------------ getters for some vars;
+
+  public boolean isSentINITIAL() {
+    return sentINITIAL;
+  }
+
+  public boolean isSentEVENT() {
+    return sentEVENT;
+  }
+
+  public boolean isReceiveEVENT() {
+    return receiveEVENT;
+  }
+
+  public boolean isSentINTERIM() {
+    return sentINTERIM;
+  }
+
+  public boolean isSentTERMINATE() {
+    return sentTERMINATE;
+  }
+
+  public boolean isReceiveINITIAL() {
+    return receiveINITIAL;
+  }
+
+  public boolean isReceiveINTERIM() {
+    return receiveINTERIM;
+  }
+
+  public boolean isReceiveTERMINATE() {
+    return receiveTERMINATE;
+  }
+
+  // ------------ getters for some vars;
+
+  @Override
+  protected int getChargingUnitsTime() {
+    return 10;
+  }
+
+  @Override
+  protected String getServiceContextId() {
+    return "tralalalal ID";
+  }
+
+}
