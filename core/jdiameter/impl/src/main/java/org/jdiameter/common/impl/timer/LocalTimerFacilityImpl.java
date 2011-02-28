d037b7d5e7d908e63ec08c365a90d4169ed09677
Alexandre Mendonça <ammendonca>
2011-02-28 10:59:36 +0000

    Pool timer objects to reduce GC work
    Update Issue 2261
    Committed provided patch.
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/diameter@17046 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- core/jdiameter/impl/src/main/java/org/jdiameter/common/impl/timer/LocalTimerFacilityImpl.java
+++ core/jdiameter/impl/src/main/java/org/jdiameter/common/impl/timer/LocalTimerFacilityImpl.java
@@ -1,7 +1,7 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @authors tag. All rights reserved.
+ * Copyright 2010, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors as indicated by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a full listing
  * of individual contributors.
  * 
@@ -26,10 +26,12 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Serializable;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.pool.BasePoolableObjectFactory;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import org.jdiameter.api.BaseSession;
 import org.jdiameter.client.api.IContainer;
 import org.jdiameter.common.api.concurrent.IConcurrentFactory;
@@ -49,13 +51,16 @@ public class LocalTimerFacilityImpl implements ITimerFacility {
 
   private static final Logger logger = LoggerFactory.getLogger(LocalTimerFacilityImpl.class);
 
-  private ScheduledExecutorService executor;
-
+  private ScheduledThreadPoolExecutor executor;
   private ISessionDatasource sessionDataSource;
+
+  // TimerTaskHandle pooling to minimize impact on Eden space and avoid too
+  // much GC, consequently not loosing time during GC
+  private final GenericObjectPool pool = new GenericObjectPool(new TimerTaskHandleFactory(), 100000, GenericObjectPool.WHEN_EXHAUSTED_GROW, 10, 20000);
 
   public LocalTimerFacilityImpl(IContainer container) {
     super();
-    this.executor = container.getConcurrentFactory().getScheduledExecutorService(IConcurrentFactory.ScheduledExecServices.ApplicationSession.name());
+    this.executor = (ScheduledThreadPoolExecutor) container.getConcurrentFactory().getScheduledExecutorService(IConcurrentFactory.ScheduledExecServices.ApplicationSession.name());
     this.sessionDataSource = container.getAssemblerFacility().getComponentInstance(ISessionDatasource.class);
   }
 
@@ -65,9 +70,14 @@ public class LocalTimerFacilityImpl implements ITimerFacility {
    * @see org.jdiameter.common.api.timer.ITimerFacility#cancel(java.io.Serializable)
    */
   public void cancel(Serializable f) {
-    if(f!= null && f instanceof TimerTaskHandle) {
-      TimerTaskHandle timerTaskHandle = (TimerTaskHandle)f;
-      timerTaskHandle.future.cancel(false);
+    if (f != null && f instanceof TimerTaskHandle) {
+      TimerTaskHandle timerTaskHandle = (TimerTaskHandle) f;
+      if (timerTaskHandle.future != null) {
+        if (executor.remove((Runnable) timerTaskHandle.future)) {
+          timerTaskHandle.future.cancel(false);
+          returnTimerTaskHandle(timerTaskHandle);
+        }
+      }
     }
   }
 
@@ -78,18 +88,57 @@ public class LocalTimerFacilityImpl implements ITimerFacility {
   public Serializable schedule(String sessionId, String timerName, long milliseconds) throws IllegalArgumentException {
     String id = sessionId + "/" + timerName;
     logger.debug("Scheduling timer with id {}", id);
-    TimerTaskHandle ir = new TimerTaskHandle();
+    TimerTaskHandle ir = borrowTimerTaskHandle();
+    ir.id = id;
     ir.sessionId = sessionId;
     ir.timerName = timerName;
     ir.future = this.executor.schedule(ir, milliseconds, TimeUnit.MILLISECONDS);
-
     return ir;
+  }
+
+  protected void returnTimerTaskHandle(TimerTaskHandle timerTaskHandle) {
+    try {
+      pool.returnObject(timerTaskHandle);
+    }
+    catch (Exception e) {
+      logger.warn(e.getMessage());
+    }
+  }
+
+  protected TimerTaskHandle borrowTimerTaskHandle() {
+    try {
+      TimerTaskHandle timerTaskHandle = (TimerTaskHandle) pool.borrowObject();
+      return timerTaskHandle;
+    }
+    catch (Exception e) {
+      logger.error("", e);
+    }
+    return null;
+  }
+
+
+
+  class TimerTaskHandleFactory extends BasePoolableObjectFactory {
+    @Override
+    public Object makeObject() throws Exception {
+      return new TimerTaskHandle();
+    }
+
+    @Override
+    public void passivateObject(Object obj) throws Exception {
+      TimerTaskHandle timerTaskHandle = (TimerTaskHandle) obj;
+      timerTaskHandle.id = null;
+      timerTaskHandle.sessionId = null;
+      timerTaskHandle.timerName = null;
+      timerTaskHandle.future = null;
+    }
   }
 
   private final class TimerTaskHandle implements Runnable, Externalizable {
     // its not really serializable;
     private String sessionId;
     private String timerName;
+    private String id; //for debug, easier to check what's going on and what that timer does.
     private transient ScheduledFuture<?> future;
 
     public void run() {
@@ -97,36 +146,43 @@ public class LocalTimerFacilityImpl implements ITimerFacility {
         BaseSession bSession = sessionDataSource.getSession(sessionId);
         if (bSession == null || !bSession.isAppSession()) {
           // FIXME: error ?
+          logger.error("Base Session is null for sessionId: {}", sessionId);
           return;
         }
         else {
-          try{
+          try {
             AppSessionImpl impl = (AppSessionImpl) bSession;
             impl.onTimer(timerName);
           }
-          catch(Exception e) {
-            logger.error("Caught exception from app session object!",e);
+          catch (Exception e) {
+            logger.error("Caught exception from app session object!", e);
           }
         }
       }
       catch (Exception e) {
-        logger.error("Failure executing timer task", e);
+        logger.error("Failure executing timer task witb id: " + id, e);
+      }
+      finally {
+        returnTimerTaskHandle(this);
       }
     }
 
-    /* 
+    /*
      * (non-Javadoc)
+     * 
      * @see java.io.Externalizable#writeExternal(java.io.ObjectOutput)
      */
+    @Override
     public void writeExternal(ObjectOutput out) throws IOException {
       throw new IOException("Failed to serialize local timer!");
-
     }
 
-    /* 
+    /*
      * (non-Javadoc)
+     * 
      * @see java.io.Externalizable#readExternal(java.io.ObjectInput)
      */
+    @Override
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
       throw new IOException("Failed to deserialize local timer!");
     }
