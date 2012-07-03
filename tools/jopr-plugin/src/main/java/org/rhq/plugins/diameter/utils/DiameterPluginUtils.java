9ca620b5160f1c2a18a7a1f633dd3c01fbe368d5
Alexandre Mendonça <ammendonca>
2012-07-03 22:02:56 +0100

    Removing tools/jopr-plugin. Present at sub-repository 'rhq-plugin'

--- tools/jopr-plugin/src/main/java/org/rhq/plugins/diameter/utils/DiameterPluginUtils.java
+++
@@ -1,7 +0,0 @@
-package org.rhq.plugins.diameter.utils;
-
-import org.rhq.core.pluginapi.inventory.ResourceComponent;
-
-public interface DiameterPluginUtils<T extends ResourceComponent> extends ResourceComponent<T> {
-  public MBeanServerUtils getMBeanServerUtils();
-}
