cafeaacac3adfe9650f71abad7ce77883b016e40
Alex <Morgan23#>
2016-09-24 21:34:29 -0300

    Sy interface implementation - api & defs - Impl missing    + fixed according fo @brainslog comments #issue 44

--- core/jdiameter/api/src/main/java/org/jdiameter/api/sy/ClientSySessionListener.java
+++ core/jdiameter/api/src/main/java/org/jdiameter/api/sy/ClientSySessionListener.java
@@ -1,44 +1,21 @@
- /*
-  * TeleStax, Open Source Cloud Communications
-  * Copyright 2011-2016, TeleStax Inc. and individual contributors
-  * by the @authors tag.
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * under the terms of the GNU Affero General Public License as
-  * published by the Free Software Foundation; either version 3 of
-  * the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU Affero General Public License for more details.
-  *
-  * You should have received a copy of the GNU Affero General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>
-  *
-  * This file incorporates work covered by the following copyright and
-  * permission notice:
-  *
-  *   JBoss, Home of Professional Open Source
-  *   Copyright 2007-2011, Red Hat, Inc. and individual contributors
-  *   by the @authors tag. See the copyright.txt in the distribution for a
-  *   full listing of individual contributors.
-  *
-  *   This is free software; you can redistribute it and/or modify it
-  *   under the terms of the GNU Lesser General Public License as
-  *   published by the Free Software Foundation; either version 2.1 of
-  *   the License, or (at your option) any later version.
-  *
-  *   This software is distributed in the hope that it will be useful,
-  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  *   Lesser General Public License for more details.
-  *
-  *   You should have received a copy of the GNU Lesser General Public
-  *   License along with this software; if not, write to the Free
-  *   Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  *   02110-1301 USA, or see the FSF site: http://www.fsf.org.
-  */
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2016, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 package org.jdiameter.api.sy;
 
@@ -47,11 +24,12 @@ import org.jdiameter.api.InternalException;
 import org.jdiameter.api.OverloadException;
 import org.jdiameter.api.RouteException;
 
+import org.jdiameter.api.auth.events.SessionTermAnswer;
+import org.jdiameter.api.auth.events.SessionTermRequest;
+
 import org.jdiameter.api.sy.events.SpendingLimitRequest;
 import org.jdiameter.api.sy.events.SpendingLimitAnswer;
-import org.jdiameter.api.sy.events.SpendingTerminationRequest;
-import org.jdiameter.api.sy.events.SpendingTerminationAnswer;
-import org.jdiameter.api.sy.events.SpendingNotificationAnswer;
+import org.jdiameter.api.sy.events.SpendingStatusNotificationRequest;
 
 /**
 * This interface defines the possible actions for the different states in the client
@@ -63,16 +41,13 @@ import org.jdiameter.api.sy.events.SpendingNotificationAnswer;
 */
 public interface ClientSySessionListener {
 
-  void doInitialSpendingLimitReportAnswer(ClientSySession session, SpendingLimitRequest request, SpendingLimitAnswer answer)
+  void doSpendingLimitAnswer(ClientSySession session, SpendingLimitRequest request, SpendingLimitAnswer answer)
       throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
 
-  void doIntermediateSpendingLimitReportAnswer(ClientSySession session, SpendingLimitRequest request, SpendingLimitAnswer answer)
+  void doFinalSpendingLimitAnswer(ClientSySession session, SessionTermRequest request, SessionTermAnswer answer)
       throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
 
-  void doFinalSpendingLimitReportAnswer(ClientSySession session, SpendingTerminationRequest request, SpendingTerminationAnswer answer)
-      throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
-
-  void doSpendingLimitReportRequest(ClientSySession session, SpendingNotificationAnswer answer)
+  void doSpendingStatusNotificationRequest(ClientSySession session, SpendingStatusNotificationRequest request)
       throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
 
 }
--- core/jdiameter/api/src/main/java/org/jdiameter/api/sy/ServerSySession.java
+++ core/jdiameter/api/src/main/java/org/jdiameter/api/sy/ServerSySession.java
@@ -1,44 +1,21 @@
- /*
-  * TeleStax, Open Source Cloud Communications
-  * Copyright 2011-2016, TeleStax Inc. and individual contributors
-  * by the @authors tag.
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * under the terms of the GNU Affero General Public License as
-  * published by the Free Software Foundation; either version 3 of
-  * the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU Affero General Public License for more details.
-  *
-  * You should have received a copy of the GNU Affero General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>
-  *
-  * This file incorporates work covered by the following copyright and
-  * permission notice:
-  *
-  *   JBoss, Home of Professional Open Source
-  *   Copyright 2007-2011, Red Hat, Inc. and individual contributors
-  *   by the @authors tag. See the copyright.txt in the distribution for a
-  *   full listing of individual contributors.
-  *
-  *   This is free software; you can redistribute it and/or modify it
-  *   under the terms of the GNU Lesser General Public License as
-  *   published by the Free Software Foundation; either version 2.1 of
-  *   the License, or (at your option) any later version.
-  *
-  *   This software is distributed in the hope that it will be useful,
-  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  *   Lesser General Public License for more details.
-  *
-  *   You should have received a copy of the GNU Lesser General Public
-  *   License along with this software; if not, write to the Free
-  *   Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  *   02110-1301 USA, or see the FSF site: http://www.fsf.org.
-  */
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2016, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 package org.jdiameter.api.sy;
 
@@ -49,9 +26,10 @@ import org.jdiameter.api.RouteException;
 import org.jdiameter.api.app.AppSession;
 import org.jdiameter.api.app.StateMachine;
 
+import org.jdiameter.api.auth.events.SessionTermAnswer;
+
 import org.jdiameter.api.sy.events.SpendingLimitAnswer;
-import org.jdiameter.api.sy.events.SpendingTerminationRequest;
-import org.jdiameter.api.sy.events.SpendingNotificationRequest;
+import org.jdiameter.api.sy.events.SpendingStatusNotificationRequest;
 
 /**
 * Basic class for Sy Server Interface specific session.
@@ -64,16 +42,13 @@ import org.jdiameter.api.sy.events.SpendingNotificationRequest;
 
 public interface ServerSySession extends AppSession, StateMachine {
 
-  void sendInitialSpendingLimitReportAnswer(SpendingLimitAnswer answer)
+  void sendSpendingLimitAnswer(SpendingLimitAnswer answer)
       throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
 
-  void sendIntermediateSpendingLimitReportAnswer(SpendingLimitAnswer request)
+  void sendFinalSpendingLimitAnswer(SessionTermAnswer answer)
       throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
 
-  void sendFinalSpendingLimitReportAnswer(SpendingTerminationRequest request)
-      throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
-
-  void sendSpendingLimitReportRequest(SpendingNotificationRequest request)
+  void sendSpendingStatusNotificationRequest(SpendingStatusNotificationRequest request)
       throws InternalException, IllegalDiameterStateException, RouteException, OverloadException;
 
 }
